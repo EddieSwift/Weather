14a51b8144005e5500d09d6a2b5ad5fc86077765
Eduard Galchenko <eddiegalchenko@gmail.com>
2019-11-09 12:34:44 +0200

    launch screen changed

--- Weather/MapViewController.swift
+++ Weather/MapViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MapViewController.swift
 //  Weather
 //
 //  Created by Eduard Galchenko on 09.11.2019.
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
 }
-
