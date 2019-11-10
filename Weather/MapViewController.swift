dca9bcc7366cf224cd0f17d627e657d963916933
Eduard Galchenko <eddiegalchenko@gmail.com>
2019-11-10 17:54:01 +0200

    MapKit and tap gestures implemented

--- Weather/MapViewController.swift
+++ Weather/MapViewController.swift
@@ -7,12 +7,92 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
-
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    let regionInMeters = 30000.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        mapView.delegate = self
+        checkLocationServices()
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapOnMap))
+        mapView.addGestureRecognizer(tapGesture)
     }
-
+    
+    @objc func tapOnMap(sender: UIGestureRecognizer){
+        print("tapOnMap")
+        
+        let locationInView = sender.location(in: mapView)
+        let locationOnMap = mapView.convert(locationInView, toCoordinateFrom: mapView)
+        print("longitude: \(locationOnMap.longitude) and latitude: \(locationOnMap.latitude)")
+        
+        self.performSegue(withIdentifier: "showWeatherSegue", sender: self)
+    }
+    
+    func addAnnotation(location: CLLocationCoordinate2D){
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Country"
+        annotation.subtitle = "City"
+        self.mapView.addAnnotation(annotation)
+    }
+    
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            setupLocationManager()
+            checkLocationAuthorization()
+        } else {
+            
+        }
+    }
+    
+    func centerViewOnUserLocation() {
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func checkLocationAuthorization() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            mapView.showsUserLocation = true
+            centerViewOnUserLocation()
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .authorizedAlways:
+            break
+        @unknown default:
+            fatalError()
+        }
+    }
+    
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showWeatherSegue" {
+            
+            if segue.destination is WeatherViewController {
+                
+            }
+        }
+    }
+    
 }
+
+
+
+
